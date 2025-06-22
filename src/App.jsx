d4d179ec770c5822ad32c2b1b247dbb32d9a2a50
tafiya <tafiapinkey@email.com>
2025-06-22 19:01:15 +0600

    set up the peoject

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+   
+      <p className=" text-5xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
